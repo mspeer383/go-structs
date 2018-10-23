608cf8dbdcf26376c3c15019843f207913176186
“Michael <michael_speer@comcast.com>
2018-10-23 11:50:11 -0500

    added triangle

--- shape_test.go
+++ shape_test.go
@@ -15,36 +15,17 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	//this converts every object that has Area() to shape
-	// thus we have a polymorphic behavior
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	}
-
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
-
 	areaTests := []struct {
 		shape Shape
 		want  float64
 	}{
 		{Rectangle{12, 6}, 72.0},
 		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
 	}
 
 	for _, tt := range areaTests {
+		//fmt.Printf("Testing shape '%#v\n'", tt)
 		got := tt.shape.Area()
 
 		if got != tt.want {
